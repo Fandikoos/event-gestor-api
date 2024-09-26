4717728f55d349297afeb04bfe8928bf226253c3
Fandikoos <danfandos@gmail.com>
2024-09-26 12:43:46 +0200

    Folding structure

---
+++ src/main/java/com/svalero/event_gestor/EventGestorApplication.java
@@ -0,0 +1,13 @@
+package com.svalero.event_gestor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventGestorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventGestorApplication.class, args);
+	}
+
+}
